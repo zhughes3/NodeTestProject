10beca3bac3eb8611849a8a95c6023db8605768a
zhughes3 <zhughes3@gmail.com>
2017-11-11 18:11:40 -0500

    added deleteAll helper

--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ module.exports = app => {
     app.get('/blog/:slug', posts.read)
     app.post('/blog', posts.create)
     app.delete('/blog/:slug', posts.delete)
+    app.get('/deleteAll', posts.deleteAll)
 }
